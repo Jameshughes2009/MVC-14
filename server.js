b333a5ce02b6ad676b57805c1ed6e0a7c9f198c4
James Hughes <jameshughes2009@hotmail.com>
2024-04-19 11:50:41 -0400

    server.js edits

--- server.js
+++ server.js
@@ -2,3 +2,6 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({helpers: require("./utils/helpers")});
+
