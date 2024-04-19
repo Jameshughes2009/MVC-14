6bf1a9d0839e3a4fb2224c2fef452cec4cb94533
James Hughes <jameshughes2009@hotmail.com>
2024-04-19 12:23:33 -0400

    sever.js and connections

--- server.js
+++ server.js
@@ -4,4 +4,22 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({helpers: require("./utils/helpers")});
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    })
+};
+
+app.use(session(sess));
 
