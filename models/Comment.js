45c4d16638ff0c72a798ba945a300c3dc666e96d
James Hughes <jameshughes2009@hotmail.com>
2024-04-19 14:42:03 -0400

    comment and user mode fixes

--- models/Comment.js
+++ models/Comment.js
@@ -9,6 +9,7 @@ Comment.init(
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
+            autoIncrement: true
         },
         user_id: {
             type: DataTypes.INTEGER,
