13d130417bf18a80d77e969e9f66bbf724eb51e2
James Hughes <jameshughes2009@hotmail.com>
2024-04-19 12:55:55 -0400

    helpers updates

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,6 +3,8 @@ module.exports = {
         const d = new Date(date);
         const currentdate =`${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`;
         const hrs = d.getHours().toString().padStart(2, '0');
-        
-    }
-}
+        const mins = d.getMinutes().toString().padStart(2,'0');
+        const currentTime = `${hrs}:${mins}`;
+        return `${currentdate} at ${currentTime}`;
+    },
+};
