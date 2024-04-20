ee229550cb2afa56782bb08e229a0fbbf458bbd0
James Hughes <jameshughes2009@hotmail.com>
2024-04-20 10:56:37 -0400

    userRoutes function

--- controller/api/userRoutes.js
+++ controller/api/userRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { json } = require('sequelize');
 const { User } = require('../../models');
 
 router.get('/', (req, res) => {
@@ -35,3 +36,28 @@ router.post('/signup', async (req, res) => {
 
 // The Functions used to login 
 
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: {username: req.body.username} });
+        if(!userData){
+            res
+                .status(400)
+                .json({ message: "Invalid Login info Please try again"});
+            return;
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword){
+            res.status(400).json({message: "Invalid Login info Please try again"});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res
+                .status(200)
+                ,json({ user: userData, message: "You have loged in"})
+        })
+    }
+})
