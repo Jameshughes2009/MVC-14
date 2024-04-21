2854155e70e6385c51eaaf97eb20b05b721f19f2
James Hughes <jameshughes2009@hotmail.com>
2024-04-21 10:30:21 -0400

    trying to fix seeds issue

---
+++ seeds/index.js
@@ -0,0 +1,18 @@
+const seedUsers = require('./userSeeds');
+const seedPosts = require('./postSeeds');
+
+
+const sequelize = require('../config/connection');
+
+//Function to seed all indo 
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true});
+    await seedUsers();
+        console.log("users seeded");
+    await seedPosts();
+    process.exit(0);
+};
+
+seedAll();
+
