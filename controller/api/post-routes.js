6531f33a106dcd92c7673df66dd18ec53bfbc33f
James Hughes <jameshughes2009@hotmail.com>
2024-04-21 09:07:31 -0400

    adding create function

--- controller/api/post-routes.js
+++ controller/api/post-routes.js
@@ -7,7 +7,25 @@ router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll({
             include: [{ model: User, attributes: ['username']}]
-        })
+        });
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
     }
-})
+});
+
+// Function to create a post 
+router.post('/', withAuth, async (req,res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
 
