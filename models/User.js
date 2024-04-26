0102cef7a73ae89add80d328bae1149c9c9b82ce
James Hughes <jameshughes2009@hotmail.com>
2024-04-26 09:10:08 -0400

    fixed the user model now will allow seeding

--- models/User.js
+++ models/User.js
@@ -1,60 +1,121 @@
-console.log("Sequelize Version:", require('sequelize').version)
+// console.log("Sequelize Version:", require('sequelize').version)
+// const { Model, DataTypes } = require('sequelize');
+// // const bcrypt = require('bcrypt');
+// const sequelize = require('../config/connection');
+// const bcryptjs = require('bcryptjs')
+
+// class User extends Model {
+//     checkPassword(loginPw) {
+//         return bcryptjs.compareSync(loginPw, this.password)
+//     }
+// }
+
+// User.init(
+//     {
+//         id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             primaryKey: true,
+//             autoIncrement: true
+//         },
+//         username: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             unique: true,
+//         },
+//         email: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             unique: true,
+//             validate: {
+//                 isEmail: true,
+//             },
+//         },
+//         password: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             validate: {
+//                 len: [8],
+//             }
+//         }
+//     },
+
+
+//     {
+//         hooks: {
+//             async beforeCreate(newUser) {
+//                 newUser.password = await bcryptjs.hash(newUser.password, 10)
+//             },
+//             async beforeUpdate(updatedUser) {
+//                 updatedUser.password = await bcryptjs.hash(updatedUser.password, 10)
+//             },
+//         },
+//         sequelize,
+//         timestamps: false,
+//         underscored: true,
+//         modelName: 'user',
+//     }
+// );
+
+// module.exports = User
+
+console.log("Sequelize version:", require('sequelize').version);
 const { Model, DataTypes } = require('sequelize');
-// const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
-const bcryptjs = require('bcryptjs')
+const bcryptjs = require('bcryptjs');
 
 class User extends Model {
-    checkPassword(loginPw) {
-        return bcryptjs.compareSync(loginPw, this.password)
-    }
+  checkPassword(loginPw) {
+    return bcryptjs.compareSync(loginPw, this.password);
+  }
 }
 
 User.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true,
-            },
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [8],
-            }
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-
-
-    {
-        hooks: {
-            async beforeCreate(newUser) {
-                newUser.password = await bcryptjs.hash(newUser.password, 10)
-            },
-            async beforeUpdate(updatedUser) {
-                updatedUser.password = await bcryptjs.hash(updatedUser.password, 10)
-            },
-        },
-        sequelize,
-        timestamps: false,
-        underscored: true,
-        modelName: 'user',
-    }
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUser) {
+        newUser.password = await bcryptjs.hash(newUser.password, 10);
+        return newUser;
+      },
+      async beforeUpdate(updatedUser) {
+        updatedUser.password = await bcryptjs.hash(updatedUser.password, 10);
+        return updatedUser;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
 );
 
-module.exports = User
+module.exports = User;
--- seeds/index.js
+++ seeds/index.js
@@ -1,3 +1,20 @@
+// const seedUsers = require('./userSeeds');
+// const seedPosts = require('./postSeeds');
+
+
+// const sequelize = require('../config/connection');
+
+// //Function to seed all indo 
+
+// const seedAll = async () => {
+//     await sequelize.sync({ force: true});
+//     await seedUsers();
+//         // console.log("users seeded");
+//     await seedPosts();
+//     process.exit(0);
+// };
+
+// seedAll();
 const seedUsers = require('./userSeeds');
 const seedPosts = require('./postSeeds');
 
@@ -15,4 +32,5 @@ const seedAll = async () => {
 };
 
 seedAll();
+
 
