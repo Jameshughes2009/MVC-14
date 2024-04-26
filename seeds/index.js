8c421c5f71fc6d4deda5e5b11560dd9465ff8f45
James Hughes <jameshughes2009@hotmail.com>
2024-04-26 10:28:29 -0400

    able to seed all comments now as well as user and posts

--- seeds/index.js
+++ seeds/index.js
@@ -17,6 +17,7 @@
 // seedAll();
 const seedUsers = require('./userSeeds');
 const seedPosts = require('./postSeeds');
+const seedComments = require('./commentSeeds');
 
 
 const sequelize = require('../config/connection');
@@ -28,6 +29,8 @@ const seedAll = async () => {
     await seedUsers();
         console.log("users seeded");
     await seedPosts();
+    await seedComments();
+        console.log ("ALL INFO SEEDED!!!")
     process.exit(0);
 };
 
